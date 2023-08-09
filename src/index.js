d321a6770739064e21c569a2006b9b0e62eb9e39
Harley Statum <eiraketh1@gmail.com>
2023-08-09 08:33:21 -0400

    completed tasks still have error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
+import { loadOptions } from '@babel/core'
 import './less/index.less'
+import { body } from 'msw/lib/types/context'
 
 // Your code goes here!
+// 1- load
+window.addEventListener('load', evt => {
+ console.log("page is fully loaded")
+})
+// 2- click
+document.body.addEventListener('click', evt => {
+ evt.target.classList.toggle('mirror')
+})
+// 3- dblclick
+document.body.addEventListener('dblclick', evt => {
+ evt.target.outerHTML = ''
+})
+// 4- mouseover
+document.body.addEventListener('mousedown', evt => {
+ const {clientX, clientY} = evt
+})
+// 5- afterprint
+window.addEventListener("afterprint", evt => {
+  console.log("After print");
+});
+// 6- error
+window.addEventListener("error", evt => {
+  log.textContent = `${log.textContent}${evt.type}: ${evt.message}\n`;
+  console.log(evt);
+});
+// 7- keydown
+window.addEventListener('keydown', evt => {
+ if (evt.key == 6) {
+  document.body.innerHTML = '<h1>YOU RAN ORDER 66</h1>'
+ }
+})
+// 8- online
+window.addEventListener("online", evt => {
+ console.log("You are now connected to the network.");
+});
+// 9- offline
+window.addEventListener("offline", evt => {
+ console.log("The network connection has been lost.");
+});
+// 10- copy
+window.addEventListener("copy", evt => {
+ console.log("copy action initiated");
+});
