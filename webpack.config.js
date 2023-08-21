e00ec01e39bdbd20e64fc6998a7c47db20e9cff7
Harley Statum <eiraketh1@gmail.com>
2023-08-21 18:22:06 -0400

    fixed maybe??

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  target: 'node',
   output: {
     filename: 'index.js',
     publicPath: '/',
